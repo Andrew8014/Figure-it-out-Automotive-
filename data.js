dd88ebbaa4668b10de8e631835bb7bde43faf0a4
britaramsay <britaramsay@gmail.com>
2026-09-25 10:15:05 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+const imgList = [
+    "images/car-repair.jpg", 
+    "images/checking-the.jpg", 
+    "images/Tire-services.jpg"
+];
+
+const makeModel = {
+    toyota: ['toyota1', 'toyota2', 'toyota3'],
+    bmw: ['bmw 1', 'bmw 2', 'bmw3', 'bmw4'],
+    honda: ['h1', 'h2', 'h3'],
+    ford: ['f1', 'f2', 'f3'],
+    "long car name": ['w1', 'long car name 3']
+}
+
+// Add accronym make and models to this.
+// First part should be all lower case, like bmw: 'BMW'
+// Put the lowercase version in the makeModel variable
+const specialNames = {
+    bmw: 'BMW',
+}
+
+const services = [
+    {
+        title: "Car Fix #1",
+        description: "hdkdh fffff sdkjfhsdcccccc dfdf ggd ggg  gfgd kdjf",
+    },
+    {
+        title: "Car Fix #2",
+        description: "This one is a lot longer and has a lot of information. sfskfsk sdjsfks gjfkgjfgf fgfg0",
+        image: "./images/carcar.jpg"
+    },
+        {
+        title: "Car Fix #3",
+        description: "hdkdh fffff sdkjfhsdcccccc dfdf ggd ggg  gfgd kdjf",
+    },
+    {
+        title: "Car Fix #4",
+        description: "This one is a lot longer and has a lot of information. sfskfsk sdjsfks gjfkgjfgf fgfg0",
+        // image: "./images/carcar.jpg"
+    },
+        {
+        title: "Car Fix #5",
+        description: "hdkdh fffff sdkjfhsdcccccc dfdf ggd ggg  gfgd kdjf",
+    },
+    {
+        title: "Car Fix #6",
+        description: "This one is a lot longer and has a lot of information. sfskfsk sdjsfks gjfkgjfgf fgfg0",
+        // image: "./images/carcar.jpg"
+    }
+];
+
+emailjsPublicKey = "FhXWaxMOEt8T9k6at";
